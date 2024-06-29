ca7769992d8a532e33160726434977f8b970c57c
Wil <sevillawil8@gmail.com>
2024-06-28 23:52:03 -0600

    cambios de adatador para deploy con vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  adapter: vercel()
 });
